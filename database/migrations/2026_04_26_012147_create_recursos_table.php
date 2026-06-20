28c197b9b2bb467730b940192aceb0e858b38d01
Job Moreno <jobmoreno98@gmail.com>
2026-06-19 21:00:04 -0600

    ajustes en el filtro

--- database/migrations/2026_04_26_012147_create_recursos_table.php
+++ database/migrations/2026_04_26_012147_create_recursos_table.php
@@ -40,7 +40,6 @@ return new class extends Migration {
 
             $table->index('coleccion_id');
             $table->index('acervo_id');
-            $table->fulltext('titulo');
         });
     }
 
